70fe1aedf582298ce0c1630f1446688320782e88
Lukwago-Robert-Kaggwa <lukwagorsaved@gmail.com>
2026-05-20 18:23:13 +0200

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Dr78Z-BE.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/admin/Desktop/Desktop/Projects/SMI/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-TKQ8ywvf.js"] }, "/": { filePath: "C:/Users/admin/Desktop/Desktop/Projects/SMI/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BN7XYFRj.js"] } }, clientEntry: "/assets/index-TKQ8ywvf.js" });
+export {
+  tsrStartManifest
+};
