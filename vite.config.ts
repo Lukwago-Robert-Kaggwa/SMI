f913b29f80f2e173583fc8fd59311365451e133d
Lukwago-Robert-Kaggwa <lukwagorsaved@gmail.com>
2026-05-20 19:25:02 +0200

    fixed base and url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/SMI/",
+  base: "/",
   plugins: [tailwindcss(), react(), tsconfigPaths()],
 });
